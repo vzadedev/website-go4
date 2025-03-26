0b06092cd7b7d5617b56af7a38db3798b24d2cf7
Pedro Antonio <pedro.morales@go4digital.com.br>
2025-03-26 09:12:28 -0300

    >

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { Inter } from 'next/font/google'
-
+import Script from 'next/script' // Importa o componente Script do Next.js
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 import type { Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'Go4Digital',
   description: 'A Go4 Digital é uma empresa especializada no desenvolvimento de soluções digitais voltadas para a Gestão de Ativos e nasceu da união de experientes profissionais do segmento de gestão de frotas.',
@@ -33,7 +33,25 @@ export default function RootLayout({
   
   return (
     <html lang="en">
-      <body className={inter.className }>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T8MQPPD9');`}
+        </Script>
+      </head>
+
+      <body className={inter.className}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8MQPPD9"
+            height="0" width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         {children}
       </body>
     </html>
